f327d5f70a671ff54d27dd157f6b292d01446819
Jeffery Patton <jefferypatton@Jeff-Pattons-MacBook-Pro.local>
2018-09-13 13:25:13 -0400

    lunch

---
+++ sort.js
@@ -0,0 +1,72 @@
+/*
+  The sort method
+
+  The built in sort method for arrays only sorts strings/numbers
+
+  We can change the default functionality of sort by passing in a callback
+
+  The structure of the callback is as follows:
+    Two parameters
+      leftItem
+      rightItem
+    A return statement
+      Numeric value of any range(negative, positive, 0)
+
+    How the sort works is by looking at the return value for any 2 given items in the array
+    If the return value is 0, negative OR falsy, we want the item on the left to be further left in the resulting array
+    If the return value is positive OR truthy, we want the item on the left to be right right in the result array
+
+    [4, 1, 6, 8, 2];
+    After 1 pass
+    [1, 4, 6, 2, 8]
+    + 1 pass
+    [1, 4, 2, 6, 8]
+
+*/
+
+let nums = [44, 1, 6, 8, 2];
+
+nums.sort(); // Does not work because it uses the lexicographic values rather than numeric
+
+// Default functionality for strings, that also works for numbers:
+// Ascending order:
+nums.sort((leftItem, rightItem) => {
+  return leftItem - rightItem;
+});
+
+nums.sort((a, b) => a - b);
+
+// Descending order
+nums.sort((leftItem, rightItem) => {
+  return rightItem - leftItem;
+});
+
+let stringAry = ['Hello', 'a', 'i', 'b', 'B'];
+
+stringAry.sort((a, b) => {
+  // Using numeric
+  if (a > b) return 1;
+  else return -1;
+});
+
+stringAry.sort((a, b) => (a > b ? 1 : -1));
+
+stringAry.sort((a, b) => a > b); // Using a boolean value
+
+// Sort an array of arrays by the length of the arrays within
+let twoDAry = [[1, 4, 21, 6, 643], [1, 5], [5], [3143, 4, 34, 4, 32, 2, 1, 3]];
+
+twoDAry.sort((a, b) => {
+  // If the length of the left item is greater than the length of the right item, we want to swap them so return a positive value OR true
+  if (a.length > b.length) {
+    return true;
+  } else {
+    // They're already in the right place, so return a negative value OR false
+    return false;
+  }
+});
+
+twoDAry.sort((a, b) => {
+  console.log(a, b);
+  return a.length > b.length;
+});
